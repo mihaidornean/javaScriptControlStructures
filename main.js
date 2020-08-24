3e864353292499d113dac7fd2b457022c7b0eaa6
mihaidornean <66698785+mihaidornean@users.noreply.github.com>
2020-08-24 22:34:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,26 +5,29 @@
 for (var i = 1; i <= 20; i++) {
     console.log(i);
 }
-console.log('2.display in the console the odd numbers from 1 to 20')
 
 //2.display in the console the odd numbers from 1 to 20
+console.log('2.display in the console the odd numbers from 1 to 20')
+
 
 for (let i = 1; i<20; i++) {
     if ( i % 2 === 1) {
         console.log(i);
     }
 }
-console.log('3.compute the sum of the elements of an array and display it in the console')
+
 // 3.compute the sum of the elements of an array and display it in the console
+console.log('3.compute the sum of the elements of an array and display it in the console')
 
 var array = [2044, 777, 120, 1500, 1115, 442, 825, 1505];
 var sum = array.reduce(function(a, b){
 return a + b; }, 0);
 console. log(sum); 
 
-console.log('4.compute the maximum of the elements of an array and display it in the console')
+
 
 // 4.compute the maximum of the elements of an array and display it in the console 
+console.log('4.compute the maximum of the elements of an array and display it in the console')
 
 function maxOfArr(arr) {
     let max = arr[0];
@@ -39,17 +42,21 @@ function maxOfArr(arr) {
 }
 let myArr = [5204, 22457, 152522, 3335550, 1121115, 445559, 85353, 1105];
 console.log(maxOfArr(myArr));
-console.log('5. compute how many times a certain element appears in an array  ')
 
 // 5. compute how many times a certain element appears in an array   
+console.log('5. compute how many times a certain element appears in an array  ')
 
-const arrayToCount = [1, 2, 3, 5, 2, 8, 9, 2, 80, 2, 84, 2 , 56, 2 , 3, 2, 62, 1000, 2, 2, 52, 4, 2];
-const result = arrayToCount.filter(i => i === 2).length;
-console.log('number of appearances ' + result);
-console.log('Challenge Nested')
+let array = [100, 2, 3, 5, 8, 9, 100, 2, 80, 100, 84, 100, 56, 100 , 3, 2, 100, 100, 100, 22, 100, 62, 1000, 2, 52, 4, 100, 100];
+let counts = {};
+
+for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+console.log("Number 100 appears " + counts[100] + " times");
 
 // Challenge Nested
-
+console.log('Challenge Nested')
 
 let a = new Array(4);
 for (let i = 0; i < 4; i++) {
